1294e4ceb5201e1bb726b6e72b9576c239ad7836
jamallatief <denaufallatief@yahoo.com>
2024-09-12 21:00:26 +0700

    update mini project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 // ini javasccript
+
+function validateluas(){
+    let panjangalas = document.getElementById('panjang-alas').value;
+    let tinggi = document.getElementById('tinggi').value;
+    
+    if(panjangalas == '' || tinggi == ''){
+        alert('Form Masih Kosong')
+    }
+    console.log(panjangalas);
+    console.log(tinggi);
+}
